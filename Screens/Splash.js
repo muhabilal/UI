47bcc1416bdef78c8e9ac5a895ecdee97a8a7b8a
M Bilal <bilaldarmuhammad@gmail.com>
2022-10-06 16:04:22 +0500

    props

--- Screens/Splash.js
+++ Screens/Splash.js
@@ -1,19 +1,13 @@
 import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
+import Button from '../Components/Button'
 
 const Splash = ({ navigation }) => {
     return (
         <View style={styles.conatiner}>
             <View style={styles.header}>
-                <Text style={styles.header_text} >Find your Gadget </Text>
-            </View>
-            <View style={styles.footer}>
-                <Image source={require('../assests/logo.png')} style={styles.image} />
-            </View>
-            <View style={styles.size}>
-                <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("login")}>
-                    <Text style={styles.text_color}>Get Started</Text>
-                </TouchableOpacity>
+                <Text style={{ color: 'white', fontSize: 60, }}>Find your</Text>
+                <Text style={{ color: 'white', fontSize: 60, }}>Gadget</Text>
             </View>
         </View>
     )
@@ -27,40 +21,8 @@ const styles = StyleSheet.create({
         backgroundColor: "#5956E9",
     },
     header: {
-        paddingVertical: 10,
-        paddingHorizontal: 30,
-        alignItems: 'center',
-    },
-    header_text: {
-        color: "#fff",
-        fontWeight: "bold",
-        fontSize: 50,
-
-    },
-    footer: {
-        paddingHorizontal: 35,
-        paddingVertical: 10,
-    },
-    image: {
-        height: 250,
-        width: '100%',
-    },
-    size: {
-        justifyContent: "center",
-        alignItems: "center",
-    },
-    button: {
-        backgroundColor: "#fff",
-        height: 40,
-        width: '50%',
-        borderRadius: 5,
-        justifyContent: "center",
-        alignItems: 'center',
-        marginTop: 30,
-    },
-    text_color: {
-        color: '#5956E9',
-        fontWeight: "bold",
-
-    },
+        flex: 0.3,
+        backgroundColor: 'red',
+        justifyContent: 'center',
+    }
 })
